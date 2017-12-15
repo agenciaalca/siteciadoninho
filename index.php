d7895ae0f8b06fd01ce5936aabf21d40b546e74d
cristiianefaria <cristiane.trc@gmail.com>
2017-12-15 06:13:38 -0200

    atualizando menu

--- index.php
+++ index.php
@@ -6,28 +6,26 @@
         <!-- Slider Section 1 -->
         <section id="home" class="slider-section">
             <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                </ol>
-
+               
                 <!-- Wrapper for slides -->
                 <div class="carousel-inner" role="listbox">
                     <div class="item active">
                         <img src="images/banners/banner_1.png" alt="...">
                         <div class="carousel-caption">
+                            <h1>Titulo</h1>
+                            <p>Descrição</p>
+                            <a href="#" class="btn btn-default">QUERO</a>
+                            <a href="#" class="btn btn-default">SAIBA MAIS</a>
                         </div>
                     </div>
+                    
                     <div class="item">
-                        <img src="images/banners/banner_2.png" alt="...">
+                        <img src="images/banners/banner_1.png" alt="...">
                         <div class="carousel-caption">
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="images/banners/banner_3.png" alt="...">
-                        <div class="carousel-caption">
+                            <h1>Titulo</h1>
+                            <p>Descrição</p>
+                            <a href="#" class="btn btn-default">QUERO</a>
+                            <a href="#" class="btn btn-default">SAIBA MAIS</a>
                         </div>
                     </div>
                 </div>
